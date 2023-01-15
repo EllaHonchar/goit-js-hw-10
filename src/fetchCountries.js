402d0a3b4bf558aa33bfff7d7a3a02ce7b6222cb
EllaHonchar <kuchincus82@gmail.com>
2023-01-15 23:17:23 +0200

    update task-10

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,20 +1,16 @@
-const BASE_URL = 'https://restcountries.com/v3.1/';
-const filters = 'name,capital,population,flags,languages'
+const BASE_URL = 'https://restcountries.com/v3.1';
+const filters = 'fields=name,capital,population,flags,languages'
 
 
 function fetchCountries(name){
-    return fetch(`${BASE_URL}/name/${name}fields=${filters}`)
+    return fetch(`${BASE_URL}/name/${name}?${filters}`)
     .then(response => {
       if (!response.ok) {
         throw new Error(response.status);
       }
       return response.json();
+      
     });
 }
-console.log(fetchCountries)
 
 export { fetchCountries };
-
-
-
-
--- src/index.js
+++ src/index.js
@@ -1,4 +1,82 @@
 import './css/styles.css';
 import { fetchCountries } from './fetchCountries';
+import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
+
+const countryInput = document.querySelector('#search-box');
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
 
 const DEBOUNCE_DELAY = 300;
+
+countryInput.addEventListener('input', debounce(onCountryInput, DEBOUNCE_DELAY));
+
+function onCountryInput () {
+    const newName = countryInput.value.trim();
+
+    if(newName === '') {
+        return (countryList.innerHTML = ''),
+        (countryInfo.innerHTML = '');
+    }
+
+
+fetchCountries(newName)
+.then(country => {
+  countryList.innerHTML = '';
+  countryInfo.innerHTML = '';
+
+  if(country.length > 10) {
+    manyMatchesAlert();
+  }
+  else if(country.length >=2 && country.length <= 10) {
+    countryList.insertAdjacentHTML('beforeend', markupCountryList(country));
+  } else {
+    countryInfo.insertAdjacentHTML('beforeend', markupCountryInfo(country));
+  }  
+})
+  .catch(wrongNameAlert);
+}
+
+
+
+function manyMatchesAlert() {
+    Notiflix.Notify.info("Too many matches found. Please enter a more specific name.");
+}
+
+function markupCountryList(country) {
+    const layoutCountryList = country.map(({flags, name}) => {
+        const layout = `
+          <li>
+              <img src="${flags.svg}" alt="Flag of ${name.official} width="250" height="200"">
+              <h2>${name.official}</h2>
+          </li>
+          `;
+      return layout;
+    })
+    .join('');
+  return layoutCountryList;
+    }
+    
+function markupCountryInfo(country) {
+    const layoutCountryInfo = country.map(({flags, name, capital, population, languages}) => {
+        const layout = `
+        <ul>
+            <li>
+              <img src="${flags.svg}" alt="Flag of ${name.official} width="250" height="200"">
+              <h2>${name.official}</h2>
+            </li>
+            <li><span>Capital: </span>${capital}</li>
+            <li><span>Population: </span>${population}</li>
+            <li>Languages: </span>${Object.values(languages,).join(', ')}</li>
+        </ul>
+        `;
+      return layout;
+    })
+    .join('');
+  return layoutCountryInfo;
+}
+
+
+function wrongNameAlert() {
+    Notiflix.Notify.failure("Oops, there is no country with that name");
+}
